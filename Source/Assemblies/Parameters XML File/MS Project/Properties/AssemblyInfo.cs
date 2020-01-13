6e129b7f28a9f01ee03ae1decfdaab3c3ce5539a
VincentRuault <vincent.ruault@ni.com>
2020-01-13 11:59:32 -0500

    This commit closes #18.

---
+++ Source/Assemblies/Parameters XML File/MS Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Parameters Arinc-429 XML Tools")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("National Instruments")]
+[assembly: AssemblyProduct("Parameters Arinc-429 XML Tools")]
+[assembly: AssemblyCopyright("Copyright © National Instruments 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("33653923-5be6-4970-b224-974e8d7fc104")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.25")]
+[assembly: AssemblyFileVersion("1.0.0.25")]
---
+++ Source/Assemblies/Parameters XML File/MS Project/XML_Tool.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Xml.Schema;
+using Microsoft.Win32;
+
+
+
+public partial class parameters
+{
+    //Function used to check XML File aginst XSD Schema.
+    static public Boolean  ValidateAgainstXMLSchema(string XMLFilepath, string SchemaXMLFilepath)
+        {
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.Schemas.Add(null, SchemaXMLFilepath);
+                settings.ValidationType = ValidationType.Schema;
+
+                XmlReader reader = XmlReader.Create(XMLFilepath, settings);
+                XmlDocument document = new XmlDocument();
+                document.Load(reader);
+
+                  document.Validate(null);
+
+                reader.Close();
+                return true;
+
+            }
+            catch (XmlSchemaValidationException ex)
+            {
+            throw new ApplicationException("\n\nError: 5000, Message: Arinc-429 XML Parameters File, Reading XML.\n\nLine Number: #" + ex.LineNumber + ", Line Position: #"+ex.LinePosition +".\n\nMessage: "+ex.Message);
+            }
+        }
+    //Function used to perform actual XML Deserialization.
+    static public parameters DeserializeXML(string XMLFilepath)
+    {
+        try
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(parameters));
+            Stream reader1 = new FileStream(XMLFilepath, FileMode.Open);
+            parameters readData = (parameters)serializer.Deserialize(reader1);
+            reader1.Close();
+            return readData;
+
+        }
+        catch (Exception ex)
+        {
+            throw new ApplicationException("\n\nError: 5001, Message: Arinc-429 XML Parameters File, XML Deserialization: " + ex.Message);
+        }
+    }
+    static public Boolean SerializeXML(string XMLFilepath, parameters Myparameters)
+    {
+        try
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(parameters));
+            Stream writer = new FileStream(XMLFilepath, FileMode.OpenOrCreate);
+            serializer.Serialize(writer,Myparameters);
+            writer.Close();
+            return true;
+
+        }
+        catch (Exception ex)
+        {
+            //throw new ApplicationException("\n\nError: 5002, Message: Arinc-429 XML Parameters File, XML Serialization: " + ex.Message);
+            throw new ApplicationException("\n\nError: 5002, Message: Arinc-429 XML Parameters File, XML Serialization: " + ex.Message+ "\nException Type: {0}"+ex.GetType().FullName+ "\nSource: {0}"+ex.Source+ "\nStrackTrace: {0}"+ex.StackTrace+"\nTargetSite: {0}"+ex.TargetSite);
+          //  Console.WriteLine("Message: {0}", ex.Message);
+            //Console.WriteLine("Exception Type: {0}", ex.GetType().FullName);
+            //Console.WriteLine("Source: {0}", ex.Source);
+            //Console.WriteLine("StrackTrace: {0}", ex.StackTrace);
+            //Console.WriteLine("TargetSite: {0}", ex.TargetSite);
+        }
+    }
+}
